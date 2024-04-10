1ef2d478474aeaea78aef64d25e1092c2d2b37c7
MASMIYEN <masmiyen@gmail.com>
2024-04-10 13:32:51 +0000

    switch to LF

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,8 @@
-function getFullResponseFromApi(success) {
+function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
         if (success) resolve({ status: 200, body: 'Success' });
         reject(Error('The fake API is not working currently'));
     });
 }
-export default getFullResponseFromApi;
+
+export default getFullResponseFromAPI;
