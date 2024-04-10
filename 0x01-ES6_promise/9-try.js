5ecbeae623543fa763187184804b850796ce1e3f
MASMIYEN <masmiyen@gmail.com>
2024-04-10 19:32:49 +0000

    sswitch to LF

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,1 @@
-export default function guardrail (mathFunction) {
-  const queue = []
-  let res
-  try {
-    res = mathFunction()
-  } catch (error) {
-    res = `Error: ${error.message}`
-  }
-  queue.push(res)
-  queue.push('Guardrail was processed')
-
-  return queue
-}
+export default function guardrail (mathFunction) {  const queue = []  let res  try {    res = mathFunction()  } catch (error) {    res = `Error: ${error.message}`  }  queue.push(res)  queue.push('Guardrail was processed')  return queue}
