a16e9d46afda3a015da3a61487bbc32b141be194
MASMIYEN <masmiyen@gmail.com>
2024-04-10 11:18:37 +0000

    another promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,7 @@
+function getFullResponseFromApi(success) {
+    return new Promise((resolve, reject) => {
+        if (success) resolve({ status: 200, body: 'Success' });
+        reject(Error('The fake API is not working currently'));
+    });
+}
+export default getFullResponseFromApi;
